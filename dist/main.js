0b522ba647fd589ff4e9cac463f8ae07d0019d81
shin donggyu <donggyu9410@gmail.com>
2019-06-04 18:43:07 +0900

    working on main page...

--- dist/main.js
+++ dist/main.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56507" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56575" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -388,4 +388,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/document.js.map
+//# sourceMappingURL=/main.js.map
